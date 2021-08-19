055e755be9437a6018e3bd57a609c5578aaa5784
Waheed Khan <wahid.khan74@gmail.com>
2021-08-19 04:19:13 +0000

    zuul setup

---
+++ src/main/java/com/dell/webservice/Phase3ZuulEdgeServer08192021Application.java
@@ -0,0 +1,13 @@
+package com.dell.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3ZuulEdgeServer08192021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3ZuulEdgeServer08192021Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dell/webservice/Phase3ZuulEdgeServer08192021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dell.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3ZuulEdgeServer08192021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
